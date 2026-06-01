b16059433dc8d8a974de3a22999bccbe129707f7
charansaigompa <gompacharansai10@gmail.com>
2026-06-01 18:21:18 +0530

    created navbar to navigate

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,71 @@
+import axios from 'axios'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../utils/constants'
+import { removeUser } from '../utils/userSlice'
+import { deleteRequest } from '../utils/requestSlice'
+import { removeConnections } from '../utils/connectoinSlice'
+import { deleteFeed } from '../utils/feedSlice'
+
+
+
+
+
+const NavBar = () => {
+  const user=useSelector(store=>store.user)
+  const dispatch=useDispatch()
+  const navigate=useNavigate()
+
+  const handleLogout=async()=>{
+    try{
+      await axios.post(BASE_URL+"/logout",{},{withCredentials:true})
+      dispatch(removeUser())
+      dispatch(deleteFeed())
+      dispatch(deleteRequest())
+      dispatch(removeConnections())
+       navigate("/login")
+
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
+
+  return (
+    <div>
+          <div className="navbar bg-base-300 shadow-sm">
+  <div className="flex-1">
+    <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
+  </div>
+{user&&(  <div className="flex gap-2">
+  <div className='flex items-center'><div> {user.firstName+" "+user.lastName}</div></div>
+   
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src={user.photoUrl} />
+        </div>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <Link to="/profile" className="justify-between">
+            Profile
+          </Link>
+        </li>
+        <li><Link to="/connections">Connections</Link></li>
+         <li><Link to="/requests">Requests</Link></li>
+        <li onClick={handleLogout}><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>)}
+</div>
+    </div>
+  )
+}
+
+export default NavBar
