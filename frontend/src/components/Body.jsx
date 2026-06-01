38ae16be589e6ae1bba622465d1017da5d4ece77
charansaigompa <gompacharansai10@gmail.com>
2026-06-01 18:25:06 +0530

    created body component to render according to route

---
+++ frontend/src/components/Body.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import NavBar from './NavBar'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Footer from './Footer'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
+import { BASE_URL } from '../utils/constants'
+
+
+
+const Body = () => {
+  const dispatch=useDispatch()
+  const navigate=useNavigate()
+  const userData=useSelector(store=>store.user
+  )
+  const fetchUser=async()=>{
+    try{
+      const user=await axios.get(BASE_URL+"/profile/view",{withCredentials:true,})
+      dispatch(addUser(user.data))
+
+
+    }
+    catch(err){
+      if(err.status===401){
+        return navigate("/login")
+      }
+      console.error(err)
+
+    }
+  }
+  useEffect(()=>{
+    if(!userData){
+ fetchUser()
+    }
+    
+  },[userData])
+  return (
+    <div>
+     <NavBar/>
+     <Outlet/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default Body
