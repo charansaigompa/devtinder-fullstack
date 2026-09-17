6bccc39f87586d9e2d52e84e3ecb78904badd7bd
charansaigompa <gompacharansai10@gmail.com>
2026-09-17 21:21:32 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const initializesocket=require("./utils/socket")
 require("dotenv").config()
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://devtinder-fullstack-1.onrender.com",
   credentials:true,
 }))
 
